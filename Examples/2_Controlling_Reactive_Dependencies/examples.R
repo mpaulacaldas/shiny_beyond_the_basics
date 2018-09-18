59e8ea1f6f038a5461cb4a0cb9726b7ce32efe62
Maria Paula Caldas <mpaulacaldas@gmail.com>
2018-09-18 18:29:36 +0200

    In examples, call packages once and avoid re-assignments to existing elements in environment

--- Examples/2_Controlling_Reactive_Dependencies/examples.R
+++ Examples/2_Controlling_Reactive_Dependencies/examples.R
@@ -1,9 +1,9 @@
+library(shiny)
+
 # Controlling Reactive Dependencies 
 
-
 # Multiple inputs, one output
-library(shiny)
-ui <- fluidPage(
+ui_01 <- fluidPage(
   textInput("title", "Title:", value = "Random normals"),
   selectInput("colour", "Select a colour", 
               choices = c("orange", "blue", "green")),
@@ -11,18 +11,33 @@ ui <- fluidPage(
                min = 10, max = 500, value = 100),
   plotOutput("histogram")
 )
-server <- function(input, output){
+server_01a <- function(input, output){
+  output$histogram <- renderPlot(
+    hist(x = rnorm(input$sampleSize),
+         main = input$title,
+         col = input$colour)
+    # Values are recalculated when we change colors
+  )
+}
+server_01b <- function(input, output){
   output$histogram <- renderPlot(
     hist(x = rnorm(input$sampleSize),
          main = isolate(input$title),
          col = isolate(input$colour))
+    # We break the dependency to the title and colour. The plot will be re-run
+    # only when the sample size is changed. The color and title will be updated
+    # only then.
   )
 }
-shinyApp(ui, server)
+
+# Protip: Google "Shiny reactive log". There is a way of getting the reactive
+# graph of an app (very messy for now, being improved by RStudio)
+
+shinyApp(ui_01, server_01a) # before isolate()
+shinyApp(ui_01, server_01b) # after isolate()
 
 # --- Preventing Dependencies with isolate -------------------------------------
-library(shiny)
-ui <- fluidPage(
+ui_02 <- fluidPage(
   textInput("title", "Title:", value = "Random normals"),
   selectInput("colour", "Select a colour", 
               choices = c("orange", "blue", "green")),
@@ -30,21 +45,21 @@ ui <- fluidPage(
                min = 10, max = 500, value = 100),
   plotOutput("histogram")
 )
-server <- function(input, output){
+server_02 <- function(input, output){
   output$histogram <- renderPlot(
     hist(x = rnorm(input$sampleSize),
          main = isolate(input$title),
          col = isolate(input$colour))
   )
 }
-shinyApp(ui, server)
+shinyApp(ui_02, server_02)
 
 # --- Defining Our Own Dependencies --------------------------------------------
 
 # --- Defining Dependencies with eventReactive
-library(shiny)
-ui <- fluidPage(
-  sliderInput("n", "Number of samples", min = 25, max = 500, step = 25, value = 100),
+ui_03 <- fluidPage(
+  sliderInput("n", "Number of samples", 
+              min = 25, max = 500, step = 25, value = 100),
   radioButtons("dist", "Distribution type:",
                c("Normal" = "norm",
                  "Uniform" = "unif",
@@ -52,9 +67,11 @@ ui <- fluidPage(
   actionButton("go", "Go"),
   plotOutput("distPlot")
 )
-server <- function(input, output) {
+server_03 <- function(input, output) {
   
   getDistData <- reactive({
+    # To get the same behaviour as eventReactive(input$go, {...}), you can call
+    # the input and use isolate()
     input$go
     isolate({
       dist <- switch(input$dist,
@@ -71,14 +88,15 @@ server <- function(input, output) {
     hist(getDistData(), col = "orange")
   })
 }
-shinyApp(ui, server)
+shinyApp(ui_03, server_03)
 
 # ------------------------------------------------------------------------------
 # Supplementary note: using eventReactive is the same as reactive + isolate
 # E.g. this app has the same dependency graph as that above.
-library(shiny)
-ui <- fluidPage(
-  sliderInput("n", "Number of samples", min = 25, max = 500, step = 25, value = 100),
+
+ui_04 <- fluidPage(
+  sliderInput("n", "Number of samples", 
+              min = 25, max = 500, step = 25, value = 100),
   radioButtons("dist", "Distribution type:",
                c("Normal" = "norm",
                  "Uniform" = "unif",
@@ -86,23 +104,23 @@ ui <- fluidPage(
   actionButton("go", "Go"),
   plotOutput("distPlot")
 )
-server <- function(input, output) {
+server_04 <- function(input, output) {
   
-  getDistData <- reactive({
-    input$go  # Take dependency
-    isolate({
+  getDistData <- eventReactive(input$go, {
       dist <- switch(input$dist,
                      norm = rnorm,
                      unif = runif,
                      exp = rexp,
                      rnorm)
       dist(input$n)
-    })
   })
+  # Conclusion: You don't need to use the eventReactive() in your apps. You
+  # could get the same behaviour using reactive() + isolate(). This last
+  # approach could however get a bit verbose and difficult to understand.
   
   output$distPlot <- renderPlot({
     hist(getDistData(), col = "orange")
   })
 }
-shinyApp(ui, server)
+shinyApp(ui_04, server_04)
 
